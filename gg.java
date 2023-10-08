3a17dcec193e818b3915191a9bb53312d0e4dede
Dmitry Ladygin <daily.hofe@gmail.com>
2023-10-08 07:36:18 +0300

    Added Features

--- gg.java
+++ gg.java
@@ -1,5 +1,11 @@
 public class gg {
    public static void main(String[] args) {
-      System.out.println("hello world!");
+      System.out.println("hello world!1");
+      System.out.println("1");
+      int x = 0;
+   }
+
+   public void go() {
+      System.out.println("go");
    }
 }
